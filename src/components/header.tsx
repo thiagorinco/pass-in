5f8ac5141db8b90a7d1d24ef728edd9bf0fe5514
Thiago Rinco da Silveira <thiago.rinco@mrv.com.br>
2024-04-02 20:44:34 -0300

    First Commit

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import logoNlw from '../assets/nlw-unite-icon.svg'
+
+export function Header() {
+    return (
+        <div className='flex items-center gap-5 py-2'>
+            <img src={logoNlw} />
+            <nav className=' flex gap-5'>
+                <a href='' className='font-medium text-sm text-zinc-300'>Eventos</a>
+                <a href='' className='font-medium text-sm'>Participantes</a>
+            </nav>
+
+        </div>
+    )
+}
